6a969e51972212fa52059dc946f58914558e699e
shademe <shadeMe@users.noreply.github.com>
2023-05-11 12:24:26 +0200

    Test all entrypoints

--- spacy_curated_transformers/tests/test_registry.py
+++ spacy_curated_transformers/tests/test_registry.py
@@ -4,6 +4,26 @@ from spacy_curated_transformers.util import registry
 
 
 @pytest.mark.parametrize(
+    "model_name",
+    [
+        "spacy-curated-transformers.AlbertTransformer.v1",
+        "spacy-curated-transformers.BertTransformer.v1",
+        "spacy-curated-transformers.CamembertTransformer.v1",
+        "spacy-curated-transformers.RobertaTransformer.v1",
+        "spacy-curated-transformers.XlmrTransformer.v1",
+        "spacy-curated-transformers.WithStridedSpans.v1",
+        "spacy-curated-transformers.ScalarWeight.v1",
+        "spacy-curated-transformers.TransformerLayersListener.v1",
+        "spacy-curated-transformers.LastTransformerLayerListener.v1",
+        "spacy-curated-transformers.ScalarWeightingListener.v1",
+    ],
+)
+def test_model_from_registry(model_name):
+    # Can't be constructed, since all models have mandatory arguments.
+    spacy_registry.architectures.get(model_name)
+
+
+@pytest.mark.parametrize(
     "encoder_name",
     [
         "spacy-curated-transformers.BertWordpieceEncoder.v1",
@@ -15,7 +35,7 @@ from spacy_curated_transformers.util import registry
         "spacy-curated-transformers.XlmrSentencepieceEncoder.v1",
     ],
 )
-def test_encoder_from_registry(encoder_name):
+def test_tokenizer_encoder_from_registry(encoder_name):
     spacy_registry.architectures.get(encoder_name)()
 
 
@@ -31,6 +51,17 @@ def test_encoder_from_registry(encoder_name):
         "spacy-curated-transformers.WordpieceLoader.v1",
     ],
 )
-def test_encoder_loader_from_registry(loader_name):
+def test_model_loaders_from_registry(loader_name):
     # Can't be constructed, since most loaders have mandatory arguments.
     registry.model_loaders.get(loader_name)
+
+
+@pytest.mark.parametrize(
+    "callback_name",
+    [
+        "spacy-curated-transformers.gradual_transformer_unfreezing.v1",
+    ],
+)
+def test_callbacks_from_registry(callback_name):
+    # Can't be constructed, since all callbacks have mandatory arguments.
+    spacy_registry.callbacks.get(callback_name)
