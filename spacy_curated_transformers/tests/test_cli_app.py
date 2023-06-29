2987a3156a2af0a3441526171967168ccc26e247
shadeMe <shadeMe@users.noreply.github.com>
2023-06-29 17:02:00 +0200

    Remove quantization support for now
    This will be reintroduced once we have a better idea of the serialization API in `curated-transformers 1.0.0`

--- spacy_curated_transformers/tests/test_cli_app.py
+++ spacy_curated_transformers/tests/test_cli_app.py
@@ -1,13 +1,6 @@
+import spacy_curated_transformers.cli.debug_pieces
 from spacy.cli import app
 from typer.testing import CliRunner
-
-import spacy_curated_transformers.cli.quantize
-import spacy_curated_transformers.cli.debug_pieces
-
-
-def test_quantize():
-    result = CliRunner().invoke(app, ["quantize-transformer", "--help"])
-    assert result.exit_code == 0
 
 
 def test_debug_pieces():
