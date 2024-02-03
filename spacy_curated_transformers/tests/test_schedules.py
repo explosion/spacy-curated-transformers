c5f83bf41f04e29602febbfde0711a4784de712e
Daniël de Kok <me@danieldk.eu>
2024-02-03 10:12:40 +0100

    Test with `curated_encoder.` as infix

--- spacy_curated_transformers/tests/test_schedules.py
+++ spacy_curated_transformers/tests/test_schedules.py
@@ -9,3 +9,4 @@ def test_schedules():
     schedule = transformer_discriminative(default_schedule, transformer_schedule)
     assert schedule(0, key=(0, "some_key")) == 1e-3
     assert schedule(0, key=(1, "curated_encoder.embeddings")) == 1e-5
+    assert schedule(0, key=(2, "wrapping_model.curated_encoder.embeddings")) == 1e-5
